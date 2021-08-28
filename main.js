10e199f42ed84478bb97258586507c58be6ac0a4
Anay-02152010 <84162960+Anay-02152010@users.noreply.github.com>
2021-08-28 11:30:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+number1=document.getElementById(number1).innerHTML;
+number2=document.getElementById(number2).innerHTML;
+
+function Area(){
+    var result=Number(document.getElementById("number1").value)*Number(document.getElementById("number2").value);
+    window.alert(result);
+  }
+  function Perimeter(){
+    var result=Number(document.getElementById("firstnumber").value)+Number(document.getElementById("firstnumber").value)+Number(document.getElementById("secondnumber").value)+Number(document.getElementById("secondnumber").value);
+    window.alert("Hellos Ms Sruti the result is: "+result);
+  }
+  
